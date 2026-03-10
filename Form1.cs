61786cfed971a3af89096bce294d03eae0243197
PetrskovskýRadim <PetrskovskRadim@V309>
2026-03-10 08:40:59 +0100

    Přidejte soubory projektu.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace PROJEKT_T3A_RP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+namespace PROJEKT_T3A_RP
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
